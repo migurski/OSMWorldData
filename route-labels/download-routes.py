3a5c5c84cf467db7f204641042d5a4dbd430bda1
Michal Migurski <mike-git@teczno.com>
2013-03-19 16:19:45 -0500

    Updated download-routes to deal with lots of tiny tables and possibly-absent modifier field

--- route-labels/download-routes.py
+++ route-labels/download-routes.py
@@ -7,6 +7,7 @@ from StringIO import StringIO
 from tempfile import mkstemp
 from itertools import count
 from bz2 import decompress
+from time import strftime
 
 import logging
 
@@ -19,25 +20,38 @@ def ogr2ogr(dbinfo, index, table, file):
     '''
     '''
     try:
-        cmd = '''ogr2ogr -t_srs mercator -nln table -overwrite
-                         -lco ENCODING=UTF-8 -lco GEOMETRY_NAME=way
+        cmd = '''ogr2ogr -t_srs <mercator> -nln <table> -overwrite
+                         -lco ENCODING=UTF-8 -lco GEOMETRY_NAME=way -nlt GEOMETRY
                          -select network,ref,modifier,zoomlevel,pixelwidth
-                         -f PostgreSQL pgconnect file'''.split()
+                         -f PostgreSQL <pgconnect> <file>'''.split()
     
         cmd[2], cmd[4], cmd[-2], cmd[-1] = mercator, table, dbinfo, file
         
         logging.debug(' '.join(cmd))
         
-        cmd = Popen(cmd)
-        cmd.wait()
+        ogr2ogr = Popen(cmd, stderr=PIPE)
+        ogr2ogr.wait()
         
-        assert cmd.returncode == 0, 'ogr2ogr returned with status=%d' % cmd.returncode
-        
-        remove(file)
+        #
+        # sometimes, modifier column is missing, try without.
+        #
+        if ogr2ogr.returncode != 0:
+            cmd[-6:-4] = '-sql', "SELECT network, ref, '' AS modifier, zoomlevel, pixelwidth FROM OgrGeoJSON"
+            
+            logging.debug(' '.join(cmd))
+            
+            ogr2ogr = Popen(cmd)
+            ogr2ogr.wait()
+            
+            assert ogr2ogr.returncode == 0, 'ogr2ogr returned with status=%d' % cmd.returncode
+            
         result = True
     
     except Exception, err:
         result = err
+    
+    finally:
+        remove(file)
 
     return index, file, table, result
 
@@ -47,6 +61,9 @@ def ogr2ogr_callback((index, file, table, result)):
     if result is True:
         logging.info('%(index)d. Imported %(file)s to %(table)s' % locals())
     else:
+        with open('errors.txt', 'a') as errors:
+            print >> errors, '%(index)d. Failed: %(result)s' % locals()
+    
         logging.info('%(index)d. Failed: %(result)s' % locals())
 
 optparser = OptionParser(usage="""%prog [options] <s3 bucket/path> <db name>
@@ -54,7 +71,7 @@ optparser = OptionParser(usage="""%prog [options] <s3 bucket/path> <db name>
 Amazon S3 connection info is expected in ~/.boto, see:
     http://code.google.com/p/boto/wiki/BotoConfig""")
 
-defaults = dict(host='localhost', user='gis', passwd=None, table='routes_altogether', loglevel=logging.INFO)
+defaults = dict(host='localhost', user='gis', passwd=None, table='routes_skeletron', srid=900913, loglevel=logging.INFO)
 
 optparser.set_defaults(**defaults)
 
@@ -67,8 +84,14 @@ optparser.add_option('-u', '--user', dest='user',
 optparser.add_option('-p', '--passwd', dest='passwd',
                      help='Postgres password, default "%(passwd)s".' % defaults)
 
+optparser.add_option('-s', '--srid', dest='srid', type='int',
+                     help='Postgres geometry SRID, default "%(srid)d".' % defaults)
+
 optparser.add_option('-t', '--table', dest='table',
                      help='Postgres results table name, default "%(table)s".' % defaults)
+
+optparser.add_option('-j', '--jobs', dest='jobs', type='int',
+                     help='Number of processing jobs, default all.')
 
 optparser.add_option('-v', '--verbose', dest='loglevel',
                      action='store_const', const=logging.DEBUG,
@@ -84,14 +107,14 @@ if __name__ == '__main__':
     
     aws_bucket, aws_prefix = aws_path.split(sep, 1)
     
-    logging.basicConfig(level=logging.INFO, format='%(levelname)08s - %(message)s')
+    logging.basicConfig(level=opts.loglevel, format='%(levelname)08s - %(message)s')
     
     s3 = connect_s3().get_bucket(aws_bucket)
     
     dbinfo = "PG:dbname='%s' host='%s' user='%s' password='%s'" % (db_name, opts.host, opts.user, opts.passwd)
     
     tables = []
-    pool = Pool()
+    pool = Pool(opts.jobs)
     
     for (key, index) in zip(s3.list(aws_prefix), count(1)):
         name = key.name
@@ -126,19 +149,15 @@ if __name__ == '__main__':
     #
     #
     
+    srid = opts.srid
     dest_table = opts.table
+    next_table = '%s_%s' % (dest_table, strftime('%Y%m%d%H%M'))
     
-    logging.info('Creating table %(dest_table)s' % locals())
+    logging.info('Creating table %(next_table)s' % locals())
 
     db = connect(host=opts.host, database=db_name, user=opts.user, password=opts.passwd).cursor()
     
-    db.execute('SELECT srid(way) FROM %s LIMIT 1' % tables[0])
-    (srid, ) = db.fetchone()
-    
-    db.execute('BEGIN')
-    db.execute('DROP TABLE IF EXISTS %(dest_table)s' % locals())
-    
-    db.execute('''CREATE TABLE %(dest_table)s
+    db.execute('''CREATE TABLE %(next_table)s
                   (
                       network    TEXT,
                       ref        TEXT,
@@ -147,24 +166,41 @@ if __name__ == '__main__':
                       pixelwidth INT
                   )''' % locals())
     
-    db.execute("SELECT AddGeometryColumn('%(dest_table)s', 'way', %(srid)d, 'LINESTRING', 2)" % locals())
+    db.execute("SELECT AddGeometryColumn('%(next_table)s', 'way', %(srid)d, 'LINESTRING', 2)" % locals())
     
     for tmp_table in tables:
-        logging.debug('Inserting from %(tmp_table)s to %(dest_table)s' % locals())
-    
-        db.execute('''INSERT INTO %(dest_table)s
-                      SELECT network, ref, modifier, zoomlevel, pixelwidth, way
-                      FROM %(tmp_table)s''' % locals())
+        logging.info('Inserting from %(tmp_table)s to %(next_table)s' % locals())
+        
+        db.execute('BEGIN')
+        db.execute('SELECT count(way) FROM %(tmp_table)s' % locals())
+        (rows, ) = db.fetchone()
+        
+        if rows:
+            db.execute('''INSERT INTO %(next_table)s
+                          SELECT network, ref, modifier, zoomlevel, pixelwidth, SetSRID((Dump(way)).geom, %(srid)d)
+                          FROM %(tmp_table)s''' % locals())
         
         db.execute('DROP TABLE %(tmp_table)s' % locals())
+        db.execute("DELETE FROM geometry_columns WHERE f_table_name = '%(tmp_table)s'" % locals())
+        db.execute('COMMIT')
     
-    logging.info('Indexing table %(dest_table)s' % locals())
+    logging.info('Indexing table %(next_table)s' % locals())
 
     # Geohash idea from http://workshops.opengeo.org/postgis-intro/clusterindex.html
-    db.execute('CREATE INDEX %(dest_table)s_geohash ON %(dest_table)s (ST_Geohash(Transform(way, 4326)))' % locals())
-    db.execute('CREATE INDEX %(dest_table)s_networks ON %(dest_table)s (network)' % locals())
-    db.execute('CLUSTER %(dest_table)s USING %(dest_table)s_geohash' % locals())
+    db.execute('CREATE INDEX %(next_table)s_gist ON %(next_table)s USING GIST (way)' % locals())
+    db.execute('CREATE INDEX %(next_table)s_geohash ON %(next_table)s (ST_Geohash(Transform(Centroid(way), 4326), 8))' % locals())
+    db.execute('CREATE INDEX %(next_table)s_networks ON %(next_table)s (network)' % locals())
+    
+    logging.info('Clustering table %(next_table)s' % locals())
 
+    db.execute('CLUSTER %(next_table)s USING %(next_table)s_geohash' % locals())
+
+    logging.info('Renaming to %(dest_table)s' % locals())
+
+    db.execute('BEGIN')
+    db.execute('DROP TABLE IF EXISTS %(dest_table)s' % locals())
+    db.execute('ALTER TABLE %(next_table)s RENAME TO %(dest_table)s' % locals())
+    db.execute("DELETE FROM geometry_columns WHERE f_table_name = '%(dest_table)s'" % locals())
+    db.execute("UPDATE geometry_columns SET f_table_name = '%(dest_table)s' WHERE f_table_name = '%(next_table)s'" % locals())
     db.execute('COMMIT')
-    db.close()
 
