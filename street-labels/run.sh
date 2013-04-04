f0bf734359c2d5b1077a6c97ea5f9b9ffc905c2b
Michal Migurski <mike-git@teczno.com>
2013-04-04 02:48:42 -0500

    Updated launch-streets with OptionParser and correct scripts in run.sh

--- street-labels/run.sh
+++ street-labels/run.sh
@@ -11,10 +11,8 @@ mkdir -p $DIR/streets-geojson-100th
 python extract-streets.py -p gis gis
 
 ln -f setup.sh $DIR/
-ln -f skeletron-hadoop-mapper.py $DIR/
+ln -f process-streets.py $DIR/
 ln -f streets-*01.json.bz2 $DIR/streets-geojson-100th/
 mv streets-*.json.bz2 $DIR/streets-geojson/
-
-curl -sL https://raw.github.com/migurski/Skeletron/master/skeletron-hadoop-reducer.py -o $DIR/skeletron-hadoop-reducer.py
 
 s3put -b osm-streets-routes-data -g public-read -p `pwd` $DIR
