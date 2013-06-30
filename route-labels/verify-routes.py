8d3c2fcfd45c30088d35e483b1cfc18d649cb718
Michal Migurski <mike-git@teczno.com>
2013-06-29 19:54:52 -0500

    Sending elapsed time metrics to cloudwatch to catch runaways

--- route-labels/verify-routes.py
+++ route-labels/verify-routes.py
@@ -1,17 +1,17 @@
 from optparse import OptionParser
 from itertools import product
 from os.path import basename
-from time import sleep
+from time import sleep, time
 from os import sep
 import logging
 
-from boto import connect_s3
+from boto import connect_s3, connect_cloudwatch
 optparser = OptionParser(usage="""%prog [options] <s3 bucket/input path> <s3 bucket/output path>
 
 Amazon S3 connection info is expected in ~/.boto, see:
     http://code.google.com/p/boto/wiki/BotoConfig""")
 
-defaults = dict(loglevel=logging.INFO)
+defaults = dict(loglevel=logging.INFO, namespace='OSM Streets and Routes')
 
 optparser.set_defaults(**defaults)
 
@@ -22,6 +22,9 @@ optparser.add_option('-v', '--verbose', dest='loglevel',
 optparser.add_option('-q', '--quiet', dest='loglevel',
                      action='store_const', const=logging.WARNING,
                      help='Output no progress information.')
+
+optparser.add_option('-n', '--namespace', dest='namespace',
+                     help='Cloudwatch namespace, default %s.' % repr(defaults['namespace']))
 
 if __name__ == '__main__':
 
@@ -34,14 +37,17 @@ if __name__ == '__main__':
     
     input_s3 = connect_s3().get_bucket(input_bucket)
     output_s3 = connect_s3().get_bucket(output_bucket)
+    cloudwatch = connect_cloudwatch()
     
     input_keys = (key for key in input_s3.list(input_prefix) if key.name.endswith('.json.bz2'))
+    start_time = time()
     
     for (key, zoom) in product(input_keys, (12, 13, 14, 15)):
         output_keyname = '%s/%d-%s' % (output_prefix.rstrip('/'), zoom, basename(key.name))
 
         while True:
             logging.debug('Looking for %s...' % output_keyname)
+            cloudwatch.put_metric_data(opts.namespace, 'Elapsed (Routes)', time() - start_time, unit='Seconds')
 
             if output_s3.get_key(output_keyname) is not None:
                 logging.info('Found %s' % output_keyname)
